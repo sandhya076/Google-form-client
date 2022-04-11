218db50091e5b2b9258624f5cd8541cde0969be0
Sandhya <lavurisandhya76@gmail.com>
2022-04-11 12:24:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Form from "./Components/Form"
+import Form from "./Components/Form1"
 function App() {
   return (
     <>
